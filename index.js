552fe96d0d392469cfdb9d519048230f9f1d7a06
Catherine Bronzo <cbronzo@mac.com>
2018-05-29 02:13:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,16 +16,12 @@ function sayHiToGrandma(string) {
      return "I can't hear you!"
     } else if (string.toUpperCase()=== string)
        return "YES INDEED!"
-     } else {
+    } else {(string === "I love you, Grandma.") {
+  return "I love you, too."
+  }
    }
 
 
-
-function sayHiToGrandma(string) {
-  if(string === "I love you, Grandma.") {
-  return "I love you, too."
-  }
-}
  
 
 
